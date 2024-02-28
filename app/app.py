214a2df5948bdf2e52a1e7c97d3a1af462fed381
raumshiba <shibani.raum@gmail.com>
2024-02-28 18:50:51 +0100

    parse and render all rewayas

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from parse import parse_quran
+from font_map import font_map
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def index():
 def quran(rewayah):
     quran = parse_quran(rewayah)
 
-    return render_template('quran.html', quran=quran)
+    return render_template('quran.html', quran=quran, rewayah=rewayah, font=font_map[rewayah])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/font_map.py
@@ -0,0 +1,10 @@
+font_map = {
+    'hafs': 'hafs18',
+    'warsh': 'warsh10',
+    'shouba': 'shouba8',
+    'qaloon': 'qaloon10',
+    'doori': 'doori9',
+    'soosi': 'soosi9',
+    'bazzi': 'bazzi7',
+    'qumbul': 'qumbul7'
+}
