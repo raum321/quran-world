6e5515e41d510e1fc57675a614cc2da088bbaea3
RaumABC <shibani.raum@gmail.com>
2024-02-29 12:18:17 +0000

    server init

---
+++ app/pythonanywhere.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from dependencies import parse_quran, return_suras, font_map
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<rewayah>')
+def quran(rewayah):
+    quran = parse_quran(rewayah)
+    suras = return_suras(quran)
+
+    return render_template('quran.html', quran=quran, suras= suras, rewayah=rewayah, font=font_map[rewayah])
